dd78a51861faa0c5026a94ee3669657cef8812ca
aalimudin <45086434+aalimudin@users.noreply.github.com>
2021-12-24 17:45:12 +0700

    Delete misc directory

--- misc/php_refactor.php
+++
@@ -1,6 +0,0 @@
-<?php
-function findFirstStringInBracket($str){
-    $firstbracket = strstr($str, '(');
-    $firstbracket = ltrim($firstbracket, '(');
-    strstr($firstbracket, ')', true);
-}
--- misc/user_tabe_query.sql
+++
@@ -1,4 +0,0 @@
-select u.id, u.name as userName, up.name as parentUserName
- from user u 
- left join user up on u.parentId = up.id 
- order by id asc
